e7227b732527477dcc730abce59df88cd9d92a61
Rafa <rafanavarroortega@gmail.com>
2026-04-25 00:00:38 +0200

    se ha añadido que se muestren la hora a la que se envían los mensajes, un fondo de pantalla al chat, algún cambio en los widths y heights de algunos elementos y un sonido a la hora de enviar los mensajes

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const boton = document.getElementById("sendBtn");
 const input = document.getElementById("messageInput");
+const audio = document.getElementById("recordBtn");
+
+sonido = new Audio("audios/Notificación Universfield.mp3");
 
 boton.addEventListener("click", () => {
   const texto = input.value;
 
   if(texto !== "") {
     const nuevoMensaje = document.createElement("div");
-    nuevoMensaje.classList.add("burbuja");
+    const fecha = new Date();
+    const divFecha = document.createElement("div");
+
+    nuevoMensaje.classList.add("mis-mensajes");
     nuevoMensaje.innerText = texto;
 
+    divFecha.classList.add("fecha");
+    divFecha.innerText = fecha.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
     messages.appendChild(nuevoMensaje);
+    nuevoMensaje.appendChild(divFecha);
 
     input.value = "";
     messages.scrollTop = messages.scrollHeight;
+
+    sonido.play();
   }
 });
 
